a5ff821e6738ff33313a5d40b42d85842a94506d
Jack Pincombe <jackpincombe1@gmail.com>
2017-10-12 14:33:14 +0100

    Adding some of the skills that I have learned from the book, Black Hat python

--- Searching Ipv4.py
+++ Searching Ipv4.py
@@ -1,5 +1,5 @@
 
-
+import time
 
 '''
 outputs all of the IP's within  a give range
@@ -8,7 +8,8 @@ outputs all of the IP's within  a give range
 
 def loop(ip):
     for i in range(1,256):
-        print(ip + '.' + str(i) )
+        print("Checking: "+ ip + '.' + str(i) )
+        time.sleep(0.1)
 
 
 loop('192.168.2')
---
+++ socket.py
@@ -0,0 +1,7 @@
+print("Making connection")
+
+for i in range(10):
+    print(".")
+
+
+print("Payload delivered")
