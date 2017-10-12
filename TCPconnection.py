2ab12257c25cd224f7729f80d18f611b3b4b65c4
Jack Pincombe <jackpincombe1@gmail.com>
2017-10-12 14:40:36 +0100

    Simple TCP code

---
+++ TCPconnection.py
@@ -0,0 +1,23 @@
+'''
+Simple script that when used will create a TCP connection
+'''
+
+
+import socket
+
+target_host = 'www.google.com'
+target_port = 80
+
+#creating a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connecting the client to the target
+client.connect(target_host,target_port)
+
+#sending some data
+client.send("GET / HTTP/1.1|r|nHost: google.com\r\n\r\n")
+
+#storing the response
+response = client.recv(4096)
+
+print(response)
