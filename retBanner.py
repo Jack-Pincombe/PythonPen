ab7110d7528374dd1286323675b9ab067d2fe287
Jack Pincombe <jackpincombe1@gmail.com>
2017-11-10 08:34:45 +0000

    Sample of a network scanner checking whether ports are open on all hosts on local network

--- retBanner.py
+++ retBanner.py
@@ -10,18 +10,31 @@ def retBanner(ip,port):
     except:
         return
 
+def checkVulns(banner):
+    if 'FreeFloat Ftp Server (Version 1.00)' in banner:
+        print('[+] FreeFloat FTP Server is vulnerable.')
+    elif '3Com 3CDaemon FTP Server Version 2.0' in banner:
+        print ('[+] 3CDaemon FTP Server is vulnerable.')
+    elif 'Ability Server 2.34' in banner:
+        print('[+] Ability FTP Server is vulnerable.')
+    elif 'Sami FTP Server 2.0.2' in banner:
+        print('[+] Sami FTP Server is vulnerable.')
+    else:
+        print('[-] FTP Server is not vulnerable.')
+    return
+
+
 def main():
-    ip1 = '192.168.95.148'
-    ip2 = '192.168.95.149'
-    port = 21
-    banner1 = retBanner(ip1,port)
-
-    if banner1:
-        print('[+]' + ip1 + ": " + banner1)
-
-    banner2 = retBanner(ip2,port)
-    if banner2:
-        print('[+]' + ip2 + ': ' + banner2)
+    portlist = [21,22,25,80,110,443]
+    for x in range(1,255):
+        ip = '192.168.1.' + str(x)
+        for port in portlist:
+            banner = retBanner(ip,port)
+            if banner:
+                print("[+]" + ip + ':' + banner)
+                checkVulns(banner)
+            else:
+                print(ip + ':' + str(port) + 'is not vulnerable!')
 
 
 if __name__ == '__main__':
